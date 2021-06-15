d10230ea3230b3040b13a8aa2e7976738d5ea946
ViktorBash <basharkevichv@gmail.com>
2021-06-15 18:26:20 -0400

    Styled search bar and part of landing page

--- stocks/forms.py
+++ stocks/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class StockSearchForm(forms.Form):
-    ticker = forms.CharField(label="", max_length=5)
+    ticker = forms.CharField(label="", max_length=5, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder' : 'Example: TSLA'}))
