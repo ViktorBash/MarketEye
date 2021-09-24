8c0189a360d5cb1cf44efc102946c3c1e6fadcb3
Ubuntu <ubuntu@ip-172-31-6-1.ec2.internal>
2021-09-24 00:07:13 +0000

    Modified stock search bar and added EC2 address to settings.py

--- marketeye/settings.py
+++ marketeye/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["34.206.54.108", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["34.206.54.108", "localhost", "127.0.0.1", "ec2-34-206-54-108.compute-1.amazonaws.com"]
 
 
 # Application definition
